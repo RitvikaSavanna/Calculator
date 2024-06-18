2447c3c7b7730dc8c034019f1d807470efbb74e3
Ritvika <ritvikasavanna@gmail.com>
2024-06-18 12:23:39 +0530

    xx

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const specialChars = ["%", "*", "/", "-", "+", "="];
 let output = "";
 
 //Define function to calculate based on button clicked.
-/*const calculate = (btnValue) => {
+const calculate = (btnValue) => {
   display.focus();
   if (btnValue === "=" && output !== "") {
     //If output has '%', replace with '/100' before evaluating.
@@ -13,24 +13,16 @@ let output = "";
     output = "";
   } else if (btnValue === "DEL") {
     //If DEL button is clicked, remove the last character from the output.
-    output = output.toString().slice(0, -1);
+    output = output.toString().slice(0,0,-1);
   } else {
     //If output is empty and button is specialChars then return
     if (output === "" && specialChars.includes(btnValue)) return;
     output += btnValue;
   }
   display.value = output;
-};*/
+};
 
 
-function allclear() {
-    lastOperation.textContent = ""
-    currentOperation.textContent = ''
-   }
-   
-   function delete1() {
-   currentOperation.textContent.slice(0,-1)
-   }
 
 //Add event listener to buttons, call calculate() on click.
 buttons.forEach((button) => {
